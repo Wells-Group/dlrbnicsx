c8b2336430e35f44fb6c0f1e76c83a8ec950e618
Nirav <niravshahcolab@gmail.com>
2023-02-02 18:21:16 +0000

    CNN example added. work on pattern like HiddenLayersNet

--- dlrbnicsx/neural_network/neural_network.py
+++ dlrbnicsx/neural_network/neural_network.py
@@ -40,6 +40,46 @@ class HiddenLayersNet(torch.nn.Module):
             result = result.detach().numpy()
         return result
 
+# TODO Implement ConvNet based demos and identify common Initialisation and pattern like for HiddenLayersNet
+class ConvNet(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(10,32,3,1) # TODO First argument (index 0) here corresponds to channel size
+        self.conv2 = torch.nn.Conv2d(32,64,3,1)
+        self.dropout1 = torch.nn.Dropout2d(0.25) # TODO inplace argument, NOTE trainign is not set here as in the train_nn and validate_nn and online_nn functions relevant model parameters are set
+        self.dropout2 = torch.nn.Dropout(0.50) # NOTE Dropout instead of Dropout2d for flattened data for compatibility in future relases
+        self.fc1 = torch.nn.Linear(9216, 128)
+        self.fc2 = torch.nn.Linear(128,10)
+
+    def forward(self,x):
+        print(x.shape)
+        x = self.conv1(x)
+        print(x.shape)
+        x = torch.nn.functional.relu(x)
+        print(x.shape)
+        x = self.conv2(x)
+        print(x.shape)
+        x = torch.nn.functional.relu(x)
+        print(f"Before max pool: {x.shape}")
+        x = torch.nn.functional.max_pool2d(x,2)
+        print(f"After max pool: {x.shape}")
+        x = self.dropout1(x)
+        print(x.shape)
+        x = torch.flatten(x,1)
+        print(x.shape)
+        x = self.fc1(x)
+        print(x.shape)
+        x = torch.nn.functional.relu(x)
+        print(x.shape)
+        x = self.dropout2(x)
+        print(x.shape)
+        x = self.fc2(x)
+        print(x.shape)
+        output = torch.nn.functional.log_softmax(x, dim=1)
+        print(output.shape)
+        return output
+
+
 '''
 input_parameter_set = np.ones([16,7]).astype("f")
 
@@ -53,5 +93,9 @@ print(type(model(input_parameter_set)))
 for param in model.parameters():
     print(param.data.dtype)
 print(input_parameter_set.dtype)
+
+NOTE for CNN:
+model = ConvNet()
+model.forward(torch.randn(3,10,28,28)) # NOTE input should be in the form (batchsize, channels, height, width), second argument (index 1) here corresponds to channel size and it should match in_channel in ConvNet class initilisation
 '''
 # NOTE np.float64 or torch.float32 datatype error. See above: astype("f")
