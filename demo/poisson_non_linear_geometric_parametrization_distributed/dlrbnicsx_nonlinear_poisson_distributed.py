621e9967c55184a6b75fd9d808521457d8bd620d
niravshah241 <niravshahcolab@gmail.com>
2023-03-14 01:29:59 +0000

    In work

--- demo/poisson_non_linear_geometric_parametrization_distributed/dlrbnicsx_nonlinear_poisson_distributed.py
+++ demo/poisson_non_linear_geometric_parametrization_distributed/dlrbnicsx_nonlinear_poisson_distributed.py
@@ -19,15 +19,18 @@ import rbnicsx.online
 
 from dlrbnicsx.neural_network.neural_network import HiddenLayersNet
 from dlrbnicsx.activation_function.activation_function_factory import Tanh
-from dlrbnicsx.dataset.custom_dataset import CustomDataset
+from dlrbnicsx.dataset.custom_partitioned_dataset \
+    import CustomPartitionedDataset
 from dlrbnicsx.interface.wrappers import DataLoader
-from dlrbnicsx.train_validate_test.train_validate_test import \
-    train_nn, validate_nn, online_nn, error_analysis
+from dlrbnicsx.train_validate_test.train_validate_test_distributed \
+    import train_nn, validate_nn, online_nn, error_analysis
 
 import matplotlib.pyplot as plt
-
+import torch.distributed as dist  # TODO
 
 # Mesh deformation class (from MDFEniCSx)
+
+
 class CustomMeshDeformation(HarmonicMeshMotion):
     def __init__(self, mesh, boundaries, bc_markers_list, bc_function_list,
                  mu, reset_reference=True, is_deformation=True):
@@ -90,7 +93,7 @@ class ProblemOnDeformedDomain(abc.ABC):
     def residual_term(self):
         return ufl.inner(ufl.exp(self._solution) * ufl.grad(self._solution),
                          ufl.grad(self._test)) * ufl.dx - \
-                         ufl.inner(self.source_term, self._test) * ufl.dx
+            ufl.inner(self.source_term, self._test) * ufl.dx
 
     @property
     def set_problem(self):
@@ -109,7 +112,7 @@ class ProblemOnDeformedDomain(abc.ABC):
         problemNonlinear = self.set_problem
         solution = dolfinx.fem.Function(self._V)
         with self._meshDeformationContext(self._mesh, self._boundaries,
-             self._boundary_markers, self._bcs_geometric, mu) as mesh_class:
+                                          self._boundary_markers, self._bcs_geometric, mu) as mesh_class:
             # solver = dolfinx.nls.petsc.NewtonSolver(self._mesh.comm,
             #                                         problemNonlinear)
             solver = dolfinx.nls.petsc.NewtonSolver(mesh_class._mesh.comm,
@@ -193,13 +196,19 @@ class PODANNReducedProblem(abc.ABC):
         return self.compute_norm(u-v)/self.compute_norm(u)
 
 
-# Read unit square mesh with Triangular elements
-mesh_comm = MPI.COMM_WORLD
+# MPI communicator variables
+world_comm = MPI.COMM_WORLD
+rank = world_comm.Get_rank()
+size = world_comm.Get_size()
+
+# Read mesh
+mesh_comm = MPI.COMM_SELF  # NOTE
 gdim = 2
 gmsh_model_rank = 0
 mesh, cell_tags, facet_tags = \
     dolfinx.io.gmshio.read_from_msh("mesh_data/mesh.msh",
                                     mesh_comm, gmsh_model_rank, gdim=gdim)
+
 
 # Mesh deformation parameters
 mu = np.array([0.3, -0.413, 4.])
@@ -208,19 +217,6 @@ problem_parametric = ProblemOnDeformedDomain(mesh, cell_tags,
                                              facet_tags,
                                              CustomMeshDeformation)
 solution_mu = problem_parametric.solve(mu)
-
-computed_file = "dlrbnicsx_solution_nonlinear_poisson/solution_computed.xdmf"
-
-with CustomMeshDeformation(mesh, facet_tags,
-                           problem_parametric._boundary_markers,
-                           problem_parametric._bcs_geometric, mu,
-                           reset_reference=True,
-                           is_deformation=True) as mesh_class:
-    with dolfinx.io.XDMFFile(mesh.comm, computed_file,
-                             "w") as solution_file:
-        solution_file.write_mesh(mesh)
-        solution_file.write_function(solution_mu)
-
 
 # POD Starts ###
 
@@ -235,29 +231,54 @@ def generate_training_set(samples=[4, 4, 4]):
     return training_set
 
 
-training_set = rbnicsx.io.on_rank_zero(mesh.comm, generate_training_set)
+# Generate samples on rank 0 and Bcast to other processes
+if rank == 0:
+    training_set = generate_training_set()
+else:
+    training_set = np.zeros_like(generate_training_set())
+
+world_comm.Bcast(training_set, root=0)
+
+training_set_indices = np.arange(rank, training_set.shape[0], size)
+
+training_set_solutions = \
+    np.zeros([training_set.shape[0], solution_mu.x.array.shape[0]])
+
+for mu_index in training_set_indices:
+    print(rbnicsx.io.TextLine(str(mu_index+1) + f"/{training_set.shape[0]}",
+                              fill="#"))
+    print(f"High fidelity solve for mu = {training_set[mu_index,:]}")
+    training_set_solutions[mu_index, :] = \
+        problem_parametric.solve(training_set[mu_index, :]).x.array
+
+
+training_set_solutions_recv = np.zeros_like(training_set_solutions)
+world_comm.Barrier()
+world_comm.Allreduce(training_set_solutions, training_set_solutions_recv,
+                     op=MPI.SUM)
 
 Nmax = 30
 
 print(rbnicsx.io.TextBox("POD offline phase begins", fill="="))
 print("")
 
-print("set up snapshots matrix")
+print("Set up snapshots matrix")
 snapshots_matrix = rbnicsx.backends.FunctionsList(problem_parametric._V)
 
-print("set up reduced problem")
+print("Set up reduced problem")
 reduced_problem = PODANNReducedProblem(problem_parametric)
 
 print("")
 
-for (mu_index, mu) in enumerate(training_set):
-    print(rbnicsx.io.TextLine(str(mu_index+1), fill="#"))
+# TODO why training_set_solutions_recv? use training set.
+for (mu_index, mu) in enumerate(training_set_solutions_recv):
+    print(rbnicsx.io.TextLine
+          (f"{mu_index+1} / {training_set_solutions_recv.shape[0]}",
+           fill="#"))
+    snapshot = dolfinx.fem.Function(problem_parametric._V)
+    snapshot.x.array[:] = training_set_solutions_recv[mu_index, :]
 
-    print("Parameter number ", (mu_index+1), "of", training_set.shape[0])
-    print("high fidelity solve for mu =", mu)
-    snapshot = problem_parametric.solve(mu)
-
-    print("update snapshots matrix")
+    print("Update snapshots matrix")
     snapshots_matrix.append(snapshot)
 
     print("")
@@ -277,22 +298,26 @@ print(rbnicsx.io.TextBox("POD-Galerkin offline phase ends", fill="="))
 positive_eigenvalues = np.where(eigenvalues > 0., eigenvalues, np.nan)
 singular_values = np.sqrt(positive_eigenvalues)
 
-plt.figure(figsize=[8, 10])
-xint = list()
-yval = list()
+print(f"Rank {rank}, Positive eigenvalues: {positive_eigenvalues}")
 
-for x, y in enumerate(eigenvalues[:len(reduced_problem._basis_functions)]):
-    yval.append(y)
-    xint.append(x+1)
+if rank == 0:
+    plt.figure(figsize=[8, 10])
+    xint = list()
+    yval = list()
 
-plt.plot(xint, yval, "*-", color="orange")
-plt.xlabel("Eigenvalue number", fontsize=18)
-plt.ylabel("Eigenvalue", fontsize=18)
-plt.xticks(xint)
-plt.yscale("log")
-plt.title("Eigenvalue decay", fontsize=24)
-plt.tight_layout()
-# plt.show()
+    for x, y in enumerate(eigenvalues[:reduced_size]):
+        yval.append(y)
+        xint.append(x+1)
+
+    plt.plot(xint, yval, "*-", color="orange")
+    plt.xlabel("Eigenvalue number", fontsize=18)
+    plt.ylabel("Eigenvalue", fontsize=18)
+    plt.xticks(xint)
+    plt.yscale("log")
+    plt.title("Eigenvalue decay", fontsize=24)
+    plt.tight_layout()
+    plt.savefig("eigenvalue_decay")
+
 
 # POD Ends ###
 
@@ -311,55 +336,87 @@ def generate_ann_input_set(samples=[4, 4, 4]):
     return training_set
 
 
-def generate_ann_output_set(problem, reduced_problem, N,
-                            input_set, mode=None):
-    output_set = np.zeros([input_set.shape[0], N])
-    for i in range(input_set.shape[0]):
+def generate_ann_output_set(problem, reduced_problem, input_set,
+                            indices, mode=None):
+    # Compute output set for ANN based on input set
+    output_set = np.zeros([input_set.shape[0],
+                           len(reduced_problem._basis_functions)])
+    rb_size = len(reduced_problem._basis_functions)
+    for i in indices:
         if mode is None:
-            print(f"Parameter number {i+1} of {input_set.shape[0]}")
-            print(f"Parameter: {input_set[i,:]}")
+            print(f"Parameter number {i+1} of ")
+            print(f"{input_set.shape[0]}: {input_set[i, :]}")
         else:
-            print(f"{mode} parameter number {i+1} of {input_set.shape[0]}")
-            print(f"Parameter: {input_set[i,:]}")
-        output_set[i, :] = \
-            reduced_problem.project_snapshot(problem.solve(input_set[i, :]),
-                                             N).array.astype("f")
+            print(f"{mode} parameter number {i+1} of ")
+            print(f"{input_set.shape[0]}: {input_set[i, :]}")
+        print(input_set[i, :])
+        solution = problem.solve(input_set[i, :])
+        print(solution.x.array)
+        output_set[i, :] = reduced_problem.project_snapshot(solution,
+                                                            rb_size).array
     return output_set
 
 
-# Training dataset
-ann_input_set = generate_ann_input_set(samples=[6, 6, 7])
-np.random.shuffle(ann_input_set)
+# Generate ANN input TRAINING samples on the rank 0 and Bcast to other processes
+if rank == 0:
+    ann_input_set = generate_ann_input_set(samples=[6, 6, 7])
+    np.random.shuffle(ann_input_set)
+else:
+    ann_input_set = \
+        np.zeros_like(generate_ann_input_set(samples=[6, 6, 7]))
+
+world_comm.Bcast(ann_input_set, root=0)
+
+# Indices for ANN dataset distribution
+indices = np.arange(rank, ann_input_set.shape[0], size)
+
 ann_output_set = generate_ann_output_set(problem_parametric, reduced_problem,
-                                         len(reduced_problem._basis_functions),
-                                         ann_input_set, mode="Training")
+                                         ann_input_set, indices, mode="Training")
+
+# Initialise zero matrix for MPI all reduce and collect output set using MPI.SUM
+ann_output_set_recv = np.zeros_like(ann_output_set)
+world_comm.Barrier()
+world_comm.Allreduce(ann_output_set, ann_output_set_recv, op=MPI.SUM)
 
 num_training_samples = int(0.7 * ann_input_set.shape[0])
 num_validation_samples = ann_input_set.shape[0] - num_training_samples
 
-reduced_problem.output_range[0] = np.min(ann_output_set)
-reduced_problem.output_range[1] = np.max(ann_output_set)
-# NOTE Output_range based on the computed values instead of user guess.
-
 input_training_set = ann_input_set[:num_training_samples, :]
-output_training_set = ann_output_set[:num_training_samples, :]
+output_training_set = ann_output_set_recv[:num_training_samples, :]
 
 input_validation_set = ann_input_set[num_training_samples:, :]
-output_validation_set = ann_output_set[num_training_samples:, :]
+output_validation_set = ann_output_set_recv[num_training_samples:, :]
 
-customDataset = CustomDataset(problem_parametric, reduced_problem,
-                              len(reduced_problem._basis_functions),
-                              input_training_set, output_training_set)
+print("\n")
+
+reduced_problem.output_range[0] = np.min(ann_output_set_recv)
+reduced_problem.output_range[1] = np.max(ann_output_set_recv)
+# NOTE Output_range based on the computed values instead of user guess.
+
+print("\n")
+
+customDataset = \
+    CustomPartitionedDataset(problem_parametric, reduced_problem,
+                             len(reduced_problem._basis_functions),
+                             input_training_set, output_training_set)
 train_dataloader = DataLoader(customDataset, batch_size=30, shuffle=True)
 
-customDataset = CustomDataset(problem_parametric, reduced_problem,
-                              len(reduced_problem._basis_functions),
-                              input_validation_set, output_validation_set)
+customDataset = \
+    CustomPartitionedDataset(problem_parametric, reduced_problem,
+                             len(reduced_problem._basis_functions),
+                             input_validation_set, output_validation_set)
 valid_dataloader = DataLoader(customDataset, shuffle=False)
 
 # ANN model
 model = HiddenLayersNet(training_set.shape[1], [30, 30],
                         len(reduced_problem._basis_functions), Tanh())
+
+for param in model.parameters():
+    # print(f"Rank {rank} \n Params before all_reduce: {param.data}")
+    # NOTE This ensures that models in all processes start with same weights and biases
+    dist.all_reduce(param.data, op=dist.ReduceOp.SUM)
+    param.data /= dist.get_world_size()
+    # print(f"Rank {rank} \n Params after all_reduce: {param.data}")
 
 # Training of ANN
 training_loss = list()
@@ -383,77 +440,93 @@ for epochs in range(max_epochs):
         break
     min_validation_loss = min(validation_loss)
 
+
 # Error analysis dataset
 print("\n")
 print("Generating error analysis (only input/parameters) dataset")
 print("\n")
-error_analysis_set = generate_ann_input_set(samples=[3, 3, 3])
-error_numpy = np.zeros(error_analysis_set.shape[0])
 
-for i in range(error_analysis_set.shape[0]):
-    print(f"Error analysis {i+1} of {error_analysis_set.shape[0]}")
-    print(f"Parameter: : {error_analysis_set[i,:]}")
+if rank == 0:
+    error_analysis_set = generate_ann_input_set(samples=[3, 3, 3])
+else:
+    error_analysis_set = \
+        np.zeros_like(generate_ann_input_set(samples=[3, 3, 3]))
+
+world_comm.Bcast(error_analysis_set, root=0)
+
+world_comm.Barrier()
+error_numpy = np.zeros(error_analysis_set.shape[0])
+error_numpy_recv = np.zeros(error_analysis_set.shape[0])
+indices = np.arange(rank, error_analysis_set.shape[0], size)
+
+for i in indices:
+    print(f"Error analysis parameter number {i+1} of ")
+    print(f"{error_analysis_set.shape[0]}: {error_analysis_set[i, :]}")
     error_numpy[i] = error_analysis(reduced_problem, problem_parametric,
                                     error_analysis_set[i, :], model,
                                     len(reduced_problem._basis_functions),
                                     online_nn, device=None)
     print(f"Error: {error_numpy[i]}")
 
+world_comm.Barrier()
+world_comm.Allreduce(error_numpy, error_numpy_recv, op=MPI.SUM)
+
 # Online phase at parameter online_mu
-online_mu = np.array([0.25, -0.3, 2.5])
-fem_solution = problem_parametric.solve(online_mu)
-rb_solution = \
-    reduced_problem.reconstruct_solution(
-        online_nn(reduced_problem, problem_parametric, online_mu, model,
-                  len(reduced_problem._basis_functions), device=None))
+if rank == 0:
+    online_mu = np.array([0.25, -0.3, 2.5])
+    fem_solution = problem_parametric.solve(online_mu)
+    rb_solution = \
+        reduced_problem.reconstruct_solution(
+            online_nn(reduced_problem, problem_parametric, online_mu, model,
+                      len(reduced_problem._basis_functions), device=None))
 
-fem_online_file \
-    = "dlrbnicsx_solution_nonlinear_poisson/fem_online_mu_computed.xdmf"
-with CustomMeshDeformation(mesh, facet_tags,
-                           problem_parametric._boundary_markers,
-                           problem_parametric._bcs_geometric,
-                           online_mu, reset_reference=True,
-                           is_deformation=True) as mesh_class:
-    with dolfinx.io.XDMFFile(mesh.comm, fem_online_file,
-                             "w") as solution_file:
-        solution_file.write_mesh(mesh)
-        solution_file.write_function(fem_solution)
+    fem_online_file \
+        = "dlrbnicsx_solution_nonlinear_poisson/fem_online_mu_computed.xdmf"
+    with CustomMeshDeformation(mesh, facet_tags,
+                               problem_parametric._boundary_markers,
+                               problem_parametric._bcs_geometric,
+                               online_mu, reset_reference=True,
+                               is_deformation=True) as mesh_class:
+        with dolfinx.io.XDMFFile(mesh.comm, fem_online_file,
+                                 "w") as solution_file:
+            solution_file.write_mesh(mesh)
+            solution_file.write_function(fem_solution)
 
-rb_online_file \
-    = "dlrbnicsx_solution_nonlinear_poisson/rb_online_mu_computed.xdmf"
-with CustomMeshDeformation(mesh, facet_tags,
-                           problem_parametric._boundary_markers,
-                           problem_parametric._bcs_geometric,
-                           online_mu, reset_reference=True,
-                           is_deformation=True) as mesh_class:
-    with dolfinx.io.XDMFFile(mesh.comm, rb_online_file,
-                             "w") as solution_file:
-        # NOTE scatter_forward not considered for online solution
-        solution_file.write_mesh(mesh)
-        solution_file.write_function(rb_solution)
+    rb_online_file \
+        = "dlrbnicsx_solution_nonlinear_poisson/rb_online_mu_computed.xdmf"
+    with CustomMeshDeformation(mesh, facet_tags,
+                               problem_parametric._boundary_markers,
+                               problem_parametric._bcs_geometric,
+                               online_mu, reset_reference=True,
+                               is_deformation=True) as mesh_class:
+        with dolfinx.io.XDMFFile(mesh.comm, rb_online_file,
+                                 "w") as solution_file:
+            # NOTE scatter_forward not considered for online solution
+            solution_file.write_mesh(mesh)
+            solution_file.write_function(rb_solution)
 
-error_function = dolfinx.fem.Function(problem_parametric._V)
-error_function.x.array[:] = \
-    fem_solution.x.array - rb_solution.x.array
-fem_rb_error_file \
-    = "dlrbnicsx_solution_nonlinear_poisson/fem_rb_error_computed.xdmf"
-with CustomMeshDeformation(mesh, facet_tags,
-                           problem_parametric._boundary_markers,
-                           problem_parametric._bcs_geometric,
-                           online_mu, reset_reference=True,
-                           is_deformation=True) as mesh_class:
-    with dolfinx.io.XDMFFile(mesh.comm, fem_rb_error_file,
-                             "w") as solution_file:
-        solution_file.write_mesh(mesh)
-        solution_file.write_function(error_function)
+    error_function = dolfinx.fem.Function(problem_parametric._V)
+    error_function.x.array[:] = \
+        fem_solution.x.array - rb_solution.x.array
+    fem_rb_error_file \
+        = "dlrbnicsx_solution_nonlinear_poisson/fem_rb_error_computed.xdmf"
+    with CustomMeshDeformation(mesh, facet_tags,
+                               problem_parametric._boundary_markers,
+                               problem_parametric._bcs_geometric,
+                               online_mu, reset_reference=True,
+                               is_deformation=True) as mesh_class:
+        with dolfinx.io.XDMFFile(mesh.comm, fem_rb_error_file,
+                                 "w") as solution_file:
+            solution_file.write_mesh(mesh)
+            solution_file.write_function(error_function)
 
-with CustomMeshDeformation(mesh, facet_tags,
-                           problem_parametric._boundary_markers,
-                           problem_parametric._bcs_geometric,
-                           online_mu, reset_reference=True,
-                           is_deformation=True) as mesh_class:
+    with CustomMeshDeformation(mesh, facet_tags,
+                               problem_parametric._boundary_markers,
+                               problem_parametric._bcs_geometric,
+                               online_mu, reset_reference=True,
+                               is_deformation=True) as mesh_class:
+        print(reduced_problem.norm_error(fem_solution, rb_solution))
+        print(reduced_problem.compute_norm(error_function))
+
     print(reduced_problem.norm_error(fem_solution, rb_solution))
     print(reduced_problem.compute_norm(error_function))
-
-print(reduced_problem.norm_error(fem_solution, rb_solution))
-print(reduced_problem.compute_norm(error_function))
---
+++ demo/poisson_non_linear_geometric_parametrization_distributed/mesh_data/mesh.py
@@ -0,0 +1,57 @@
+import gmsh
+
+from mpi4py import MPI
+
+import dolfinx
+
+gmsh.initialize('', False)
+
+lc = 0.04
+gdim = 2
+
+gmsh.model.geo.addPoint(0., 0., 0., lc, 1)
+gmsh.model.geo.addPoint(1., 0., 0., lc, 2)
+gmsh.model.geo.addPoint(1., 1., 0., lc, 3)
+gmsh.model.geo.addPoint(0., 1., 0., lc, 4)
+
+gmsh.model.geo.addLine(1, 2, 1)
+gmsh.model.geo.addLine(2, 3, 2)
+gmsh.model.geo.addLine(3, 4, 3)
+gmsh.model.geo.addLine(4, 1, 4)
+
+gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
+gmsh.model.geo.addPlaneSurface([1], 1)
+
+gmsh.model.geo.synchronize()
+
+gmsh.model.mesh.generate(gdim)
+gmsh.model.mesh.setOrder(2)
+gmsh.model.mesh.optimize("Netgen")
+
+# Extract edges and surfaces to add physical groups
+surfaces = gmsh.model.getEntities(dim=gdim)
+edges = gmsh.model.getBoundary(surfaces)
+
+# Subdomains markings
+for i in range(1, len(surfaces)+1):
+    gmsh.model.addPhysicalGroup(gdim, [surfaces[i-1][1]], surfaces[i-1][1])
+# External boundaries markings
+for i in range(1, len(edges)+1):
+    gmsh.model.addPhysicalGroup(gdim-1, [edges[i-1][1]], edges[i-1][1])
+
+gmsh.write("mesh.msh")
+
+gmsh.fltk.run()
+
+gmsh.finalize()
+
+# Import mesh in dolfinx
+gmsh_model_rank = 0
+mesh_comm = MPI.COMM_WORLD
+mesh, cell_tags, facet_tags = dolfinx.io.gmshio.read_from_msh(
+    "mesh.msh", mesh_comm, gmsh_model_rank, gdim=gdim)
+
+with dolfinx.io.XDMFFile(MPI.COMM_WORLD, "mesh.xdmf", "w") as mesh_file_xdmf:
+    mesh_file_xdmf.write_mesh(mesh)
+    mesh_file_xdmf.write_meshtags(cell_tags)
+    mesh_file_xdmf.write_meshtags(facet_tags)
