b1ea69166497258f5443f7b20206f59eb711c037
niravshah241 <niravshahcolab@gmail.com>
2023-05-25 23:29:59 +0100

    POD multiprocess added

--- demo/poisson_non_linear_geometric_parametrization_multiprocess/dlrbnicsx_pod_multiprocess_2.py
+++ demo/poisson_non_linear_geometric_parametrization_multiprocess/dlrbnicsx_pod_multiprocess_2.py
@@ -1,7 +1,7 @@
 import abc
 import ufl
 import dolfinx
-from petsc4py import PETSc
+# from petsc4py import PETSc
 from mpi4py import MPI
 import numpy as np
 import itertools
@@ -12,25 +12,27 @@ import rbnicsx.online
 
 from mdfenicsx.mesh_motion_classes import HarmonicMeshMotion
 
+
 class CustomMeshDeformation(HarmonicMeshMotion):
     def __init__(self, mesh, boundaries, boundary_markers,
-                 bc_function_list, mu, reset_reference = True,
-                 is_deformation = True):
-        super().__init__(mesh, boundaries,boundary_markers,
+                 bc_function_list, mu, reset_reference=True,
+                 is_deformation=True):
+        super().__init__(mesh, boundaries, boundary_markers,
                          bc_function_list, reset_reference,
                          is_deformation)
         self.mu = mu
-    
+
     def __enter__(self):
         gdim = self._mesh.geometry.dim
         mu = self.mu
         self.shape_parametrization = self.solve()
         self._mesh.geometry.x[:, 0] += \
             (mu[2] - 1) * self._mesh.geometry.x[:, 0]
-        self._mesh.geometry.x[:,:gdim] += \
+        self._mesh.geometry.x[:, :gdim] += \
             self.shape_parametrization.x.array.\
-                reshape(self._reference_coordinates.shape[0], gdim)
+            reshape(self._reference_coordinates.shape[0], gdim)
         self._mesh.geometry.x[:, 0] -= min(self._mesh.geometry.x[:, 0])
+
 
 class ProblemOnDeformedDomain(abc.ABC):
     def __init__(self, mesh, subdomains, boundaries, meshDeformationContext):
@@ -50,7 +52,7 @@ class ProblemOnDeformedDomain(abc.ABC):
             rbnicsx.backends.bilinear_form_action(self._inner_product,
                                                   part="real")
         self._solution = dolfinx.fem.Function(self._V)
-        
+
     @property
     def assemble_bcs(self):
         bcs = list()
@@ -60,26 +62,26 @@ class ProblemOnDeformedDomain(abc.ABC):
                     self._V, self.gdim - 1, self._boundaries.find(i))
             bcs.append(dolfinx.fem.dirichletbc(self._dirichletFunc, dofs))
         return bcs
-    
+
     @property
     def source_term(self):
-        return -ufl.div(ufl.exp(self._dirichletFunc) * \
-            ufl.grad(self._dirichletFunc))
-    
+        return -ufl.div(ufl.exp(self._dirichletFunc) *
+                        ufl.grad(self._dirichletFunc))
+
     @property
     def residual_term(self):
-        return ufl.inner(ufl.exp(self._solution) * ufl.grad(self._solution), 
-                            ufl.grad(self.test)) * ufl.dx - \
-                ufl.inner(self.source_term, self.test) * ufl.dx
-    
+        return ufl.inner(ufl.exp(self._solution) * ufl.grad(self._solution),
+                         ufl.grad(self.test)) * ufl.dx - \
+            ufl.inner(self.source_term, self.test) * ufl.dx
+
     @property
     def set_problem(self):
         problemNonlinear = \
             dolfinx.fem.petsc.NonlinearProblem(self.residual_term,
-                                                self._solution,
-                                                bcs=self.assemble_bcs)
+                                               self._solution,
+                                               bcs=self.assemble_bcs)
         return problemNonlinear
-    
+
     def solve(self, mu):
         self._bcs_geometric = \
             [lambda x: (0. * x[0], mu[0] * np.sin(x[0] * np.pi)),
@@ -114,10 +116,10 @@ world_comm = MPI.COMM_WORLD
 world_rank = world_comm.rank
 world_size = world_comm.size
 
-group0_procs = world_comm.group.Incl([0, 1, 2, 3]) # world_comm.group.Incl([0, 1])
+group0_procs = world_comm.group.Incl([0, 1, 2, 3])  # world_comm.group.Incl([0, 1])
 gpu_group0_comm = world_comm.Create_group(group0_procs)
 
-group1_procs = world_comm.group.Incl([4, 5, 6, 7]) # world_comm.group.Incl([2, 3])
+group1_procs = world_comm.group.Incl([4, 5, 6, 7])  # world_comm.group.Incl([2, 3])
 gpu_group1_comm = world_comm.Create_group(group1_procs)
 
 gpu_comm_list = [gpu_group0_comm, gpu_group1_comm]
@@ -130,20 +132,20 @@ for i in range(len(gpu_comm_list)):
 
 if gpu_group0_comm != MPI.COMM_NULL:
 
-    fem0_procs = gpu_group0_comm.group.Incl([0, 1]) # gpu_group0_comm.group.Incl([0])
+    fem0_procs = gpu_group0_comm.group.Incl([0, 1])  # gpu_group0_comm.group.Incl([0])
     fem0_procs_comm = gpu_group0_comm.Create_group(fem0_procs)
 
-    fem1_procs = gpu_group0_comm.group.Incl([2, 3]) # gpu_group0_comm.group.Incl([1])
+    fem1_procs = gpu_group0_comm.group.Incl([2, 3])  # gpu_group0_comm.group.Incl([1])
     fem1_procs_comm = gpu_group0_comm.Create_group(fem1_procs)
 
     cuda_num = 0
 
 if gpu_group1_comm != MPI.COMM_NULL:
 
-    fem2_procs = gpu_group1_comm.group.Incl([0, 1]) # gpu_group1_comm.group.Incl([0])
+    fem2_procs = gpu_group1_comm.group.Incl([0, 1])  # gpu_group1_comm.group.Incl([0])
     fem2_procs_comm = gpu_group1_comm.Create_group(fem2_procs)
 
-    fem3_procs = gpu_group1_comm.group.Incl([2, 3]) # gpu_group1_comm.group.Incl([1])
+    fem3_procs = gpu_group1_comm.group.Incl([2, 3])  # gpu_group1_comm.group.Incl([1])
     fem3_procs_comm = gpu_group1_comm.Create_group(fem3_procs)
 
     cuda_num = 1
@@ -158,21 +160,21 @@ if gpu_group0_comm != MPI.COMM_NULL:
     if fem1_procs_comm != MPI.COMM_NULL:
         print(
             f"Group 0: World rank {world_rank}, FEM 1 rank {fem1_procs_comm.rank}, Cuda number {cuda_num}")
-        mu = np.array([0.2, -0.2, 1.])# np.array([0.3, -0.25, 1.5])
+        mu = np.array([0.2, -0.2, 1.])  # np.array([0.3, -0.25, 1.5])
         mesh_comm = fem1_procs_comm
-    
+
     fem_comm_list = [fem0_procs_comm, fem1_procs_comm]
 
 if gpu_group1_comm != MPI.COMM_NULL:
     if fem2_procs_comm != MPI.COMM_NULL:
         print(
             f"Group 1: World rank {world_rank}, FEM 2 rank {fem2_procs_comm.rank}, Cuda number {cuda_num}")
-        mu = np.array([0.2, -0.2, 1.]) #np.array([0.28, -0.3, 2.3])
+        mu = np.array([0.2, -0.2, 1.])  # np.array([0.28, -0.3, 2.3])
         mesh_comm = fem2_procs_comm
     if fem3_procs_comm != MPI.COMM_NULL:
         print(
             f"Group 1: World rank {world_rank}, FEM 3 rank {fem3_procs_comm.rank}, Cuda number {cuda_num}")
-        mu = np.array([0.2, -0.2, 1.]) #np.array([0.22, -0.4, 3.])
+        mu = np.array([0.2, -0.2, 1.])  # np.array([0.22, -0.4, 3.])
         mesh_comm = fem3_procs_comm
 
     fem_comm_list = [fem2_procs_comm, fem3_procs_comm]
@@ -196,14 +198,16 @@ itemsize = MPI.DOUBLE.Get_size()
 num_snapshots = 64
 para_dim = 3
 
+
 def generate_training_set(samples=[4, 4, 4]):
     training_set_0 = np.linspace(0.2, 0.3, samples[0])
     training_set_1 = np.linspace(-0.2, -0.3, samples[1])
     training_set_2 = np.linspace(1., 4., samples[2])
     training_set = np.array(list(itertools.product(training_set_0,
-                                                training_set_1,
-                                                training_set_2)))
+                                                   training_set_1,
+                                                   training_set_2)))
     return training_set
+
 
 if world_comm.rank == 0:
     nbytes = num_snapshots * para_dim * itemsize
@@ -276,11 +280,12 @@ print(rbnicsx.io.TextLine("perform POD", fill="#"))
 reduced_inner_product_action = \
     rbnicsx.backends.bilinear_form_action(problem._inner_product,
                                           part="real")
-eigenvalues, modes, _ = rbnicsx.backends.proper_orthogonal_decomposition(snapshots_matrix, reduced_inner_product_action, N=Nmax, tol=1e-6)
+eigenvalues, modes, _ = rbnicsx.backends.proper_orthogonal_decomposition(
+    snapshots_matrix, reduced_inner_product_action, N=Nmax, tol=1e-6)
 
-# print(f"Rank {world_comm.rank}, Eigenvalues: {eigenvalues[:Nmax]}")                                                                                                                                                                                                                                                        
+# print(f"Rank {world_comm.rank}, Eigenvalues: {eigenvalues[:Nmax]}")
 
-func_empty = dolfinx.fem.Function(problem._V)                                                                                                                                                                                   
+func_empty = dolfinx.fem.Function(problem._V)
 rstart, rend = func_empty.vector.getOwnershipRange()
 num_dofs = mesh.comm.allreduce(rend, op=MPI.MAX) - mesh.comm.allreduce(rstart, op=MPI.MIN)
 
@@ -309,11 +314,13 @@ reduced_inner_product_action = \
     rbnicsx.backends.bilinear_form_action(problem._inner_product,
                                           part="real")
 
+
 def compute_inner_product(fun_j):
     def inner_func(fun_i):
         return fun_i.vector.dot(fun_j.vector)
 
     return inner_func
+
 
 for j in range(len(fem_comm_list)):
     if fem_comm_list[j] != MPI.COMM_NULL:
