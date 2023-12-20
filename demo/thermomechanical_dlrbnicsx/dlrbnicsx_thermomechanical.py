44d24e286c756e1c0c99309e0bc40191274a5fea
niravshah241 <niravshahcolab@gmail.com>
2023-12-20 12:58:06 +0000

    In work

--- demo/thermomechanical_dlrbnicsx/dlrbnicsx_thermomechanical.py
+++ demo/thermomechanical_dlrbnicsx/dlrbnicsx_thermomechanical.py
@@ -310,242 +310,9 @@ error_analysis_samples = [2, 2, 2, 2]
 thermal_problem_parametric = \
     ThermalProblemOnDeformedDomain(mesh, cell_tags, facet_tags)
 
-'''
-# solution_mu = thermal_problem_parametric.solve(mu_ref)
-# print(f"Solution norm at mu:{mu_ref}: {thermal_problem_parametric.inner_product_action(solution_mu)(solution_mu)}")
-
-solution_mu = thermal_problem_parametric.solve(mu)
-print(f"Solution norm at mu:{mu}: {thermal_problem_parametric.inner_product_action(solution_mu)(solution_mu)}")
-
-computed_file = "solution_nonlinear_thermomechanical_thermal/solution_computed.xdmf"
-
-VT_plot = dolfinx.fem.FunctionSpace(mesh, ("CG", mesh.geometry.cmaps[0].degree))
-uT_func_plot = dolfinx.fem.Function(VT_plot)
-uT_func_plot.interpolate(solution_mu)
-with MeshDeformationWrapperClass(mesh, facet_tags, mu_ref, mu):
-    with dolfinx.io.XDMFFile(mesh.comm, computed_file, "w") as solution_file:
-        solution_file.write_mesh(mesh)
-        solution_file.write_function(uT_func_plot)
-'''
-# Thermal POD Starts ###
-
-def generate_training_set(samples=pod_samples):
-    training_set_0 = np.linspace(0.55, 0.75, samples[0])
-    training_set_1 = np.linspace(0.35, 0.55, samples[1])
-    training_set_2 = np.linspace(0.8, 1.2, samples[2])
-    training_set_3 = np.linspace(0.4, 0.6, samples[3])
-    training_set = np.array(list(itertools.product(training_set_0,
-                                                   training_set_1,
-                                                   training_set_2,
-                                                   training_set_3)))
-    return training_set
-
-'''
-thermal_training_set = rbnicsx.io.on_rank_zero(mesh.comm, generate_training_set)
-
-Nmax = 30
-
-print(rbnicsx.io.TextBox("POD offline phase begins", fill="="))
-print("")
-
-print("set up snapshots matrix")
-thermal_snapshots_matrix = rbnicsx.backends.FunctionsList(thermal_problem_parametric._VT)
-
-print("set up reduced problem")
-thermal_reduced_problem = ThermalPODANNReducedProblem(thermal_problem_parametric)
-
-print("")
-
-for (mu_index, mu) in enumerate(thermal_training_set):
-    print(rbnicsx.io.TextLine(str(mu_index+1), fill="#"))
-    print("Parameter number ", (mu_index+1), "of", thermal_training_set.shape[0])
-    print("high fidelity solve for mu =", mu)
-    snapshot = thermal_problem_parametric.solve(mu)
-
-    print("update snapshots matrix")
-    thermal_snapshots_matrix.append(snapshot)
-
-    print("")
-
-print(rbnicsx.io.TextLine("perform POD", fill="#"))
-thermal_eigenvalues, thermal_modes, _ = \
-    rbnicsx.backends.\
-    proper_orthogonal_decomposition(thermal_snapshots_matrix,
-                                    thermal_reduced_problem._inner_product_action,
-                                    N=Nmax, tol=1.e-10)
-thermal_reduced_problem._basis_functions.extend(thermal_modes)
-thermal_reduced_size = len(thermal_reduced_problem._basis_functions)
-print("")
-
-print(rbnicsx.io.TextBox("POD-Galerkin offline phase ends", fill="="))
-
-thermal_positive_eigenvalues = np.where(thermal_eigenvalues > 0., thermal_eigenvalues, np.nan)
-thermal_singular_values = np.sqrt(thermal_positive_eigenvalues)
-
-plt.figure(figsize=[8, 10])
-xint = list()
-yval = list()
-
-for x, y in enumerate(thermal_eigenvalues[:Nmax]):
-    yval.append(y)
-    xint.append(x+1)
-
-plt.plot(xint, yval, "*-", color="orange")
-plt.xlabel("Eigenvalue number", fontsize=18)
-plt.ylabel("Eigenvalue", fontsize=18)
-plt.xticks(xint)
-plt.yscale("log")
-plt.title("Eigenvalue decay (Thermal)", fontsize=24)
-plt.tight_layout()
-plt.savefig("thermal_eigenvalues.png")
-
-print(f"Eigenvalues (Thermal): {thermal_positive_eigenvalues}")
-'''
-# Thermal POD Ends ###
-
-# 5. ANN implementation
-
-def generate_ann_input_set(samples=ann_samples):
-    """Generate an equispaced training set using numpy."""
-    training_set_0 = np.linspace(0.55, 0.75, samples[0])
-    training_set_1 = np.linspace(0.35, 0.55, samples[1])
-    training_set_2 = np.linspace(0.8, 1.2, samples[2])
-    training_set_3 = np.linspace(0.4, 0.6, samples[3])
-    training_set = np.array(list(itertools.product(training_set_0,
-                                                   training_set_1,
-                                                   training_set_2,
-                                                   training_set_3)))
-    return training_set
-
-
-def generate_ann_output_set(problem, reduced_problem,
-                            input_set, mode=None):
-    output_set = np.zeros([input_set.shape[0],
-                           len(reduced_problem._basis_functions)])
-    for i in range(input_set.shape[0]):
-        if mode is None:
-            print(f"Parameter number {i+1} of {input_set.shape[0]}")
-            print(f"Parameter: {input_set[i,:]}")
-        else:
-            print(f"{mode} parameter number {i+1} of {input_set.shape[0]}")
-            print(f"Parameter: {input_set[i,:]}")
-        output_set[i, :] = \
-            reduced_problem.project_snapshot(problem.solve(input_set[i, :]),
-                                             len(reduced_problem._basis_functions)).array.astype("f")
-    return output_set
-
-'''
-# Training dataset
-thermal_ann_input_set = generate_ann_input_set(samples=ann_samples)
-# np.random.shuffle(thermal_ann_input_set)
-thermal_ann_output_set = \
-    generate_ann_output_set(thermal_problem_parametric,
-                            thermal_reduced_problem,
-                            thermal_ann_input_set, mode="Training")
-
-thermal_num_training_samples = int(0.7 * thermal_ann_input_set.shape[0])
-thermal_num_validation_samples = \
-    thermal_ann_input_set.shape[0] - thermal_num_training_samples
-
-thermal_reduced_problem.output_range[0] = np.min(thermal_ann_output_set)
-thermal_reduced_problem.output_range[1] = np.max(thermal_ann_output_set)
-# NOTE Output_range based on the computed values instead of user guess.
-
-thermal_input_training_set = thermal_ann_input_set[:thermal_num_training_samples, :]
-thermal_output_training_set = thermal_ann_output_set[:thermal_num_training_samples, :]
-
-thermal_input_validation_set = thermal_ann_input_set[thermal_num_training_samples:, :]
-thermal_output_validation_set = thermal_ann_output_set[thermal_num_training_samples:, :]
-
-customDataset = CustomDataset(thermal_reduced_problem,
-                              thermal_input_training_set, thermal_output_training_set)
-thermal_train_dataloader = DataLoader(customDataset, batch_size=40, shuffle=False)# shuffle=True)
-
-customDataset = CustomDataset(thermal_reduced_problem,
-                              thermal_input_validation_set, thermal_output_validation_set)
-thermal_valid_dataloader = DataLoader(customDataset, shuffle=False)
-
-# ANN model
-thermal_model = HiddenLayersNet(thermal_training_set.shape[1],
-                                [35, 35],
-                                len(thermal_reduced_problem._basis_functions),
-                                Tanh())
-
-for params in thermal_model.parameters():
-    print(params.shape)
-
-thermal_path = "thermal_model.pth"
-save_model(thermal_model, thermal_path)
-# load_model(thermal_model, thermal_path)
-
-
-# Training of ANN
-thermal_training_loss = list()
-thermal_validation_loss = list()
-
-thermal_max_epochs = 20 #000
-thermal_min_validation_loss = None
-thermal_start_epoch = 0
-thermal_checkpoint_path = "thermal_checkpoint"
-thermal_checkpoint_epoch = 10
-
-thermal_learning_rate = 1e-4
-thermal_optimiser = get_optimiser(thermal_model, "Adam", thermal_learning_rate)
-thermal_loss_fn = get_loss_func("MSE", reduction="sum")
-
-if os.path.exists(thermal_checkpoint_path):
-    thermal_start_epoch, thermal_min_validation_loss = \
-        load_checkpoint(thermal_checkpoint_path, thermal_model, thermal_optimiser)
-
-import time
-start_time = time.time()
-for thermal_epochs in range(thermal_start_epoch, thermal_max_epochs):
-    if thermal_epochs > 0 and thermal_epochs % thermal_checkpoint_epoch == 0:
-        save_checkpoint(thermal_checkpoint_path, thermal_epochs, thermal_model, thermal_optimiser,
-                        thermal_min_validation_loss)
-    print(f"Epoch: {thermal_epochs+1}/{thermal_max_epochs}")
-    thermal_current_training_loss = train_nn(thermal_reduced_problem, thermal_train_dataloader,
-                                     thermal_model, thermal_loss_fn, thermal_optimiser)
-    thermal_training_loss.append(thermal_current_training_loss)
-    thermal_current_validation_loss = validate_nn(thermal_reduced_problem, thermal_valid_dataloader,
-                                          thermal_model, thermal_loss_fn)
-    thermal_validation_loss.append(thermal_current_validation_loss)
-    if thermal_epochs > 0 and thermal_current_validation_loss > 1.01 * thermal_min_validation_loss \
-       and thermal_reduced_problem.regularisation == "EarlyStopping":
-        # 1% safety margin against min_validation_loss
-        # before invoking early stopping criteria
-        print(f"Early stopping criteria invoked at epoch: {thermal_epochs+1}")
-        break
-    thermal_min_validation_loss = min(thermal_validation_loss)
-end_time = time.time()
-thermal_elapsed_time = end_time - start_time
-
-os.system(f"rm {thermal_checkpoint_path}")
-
-# Error analysis dataset
-print("\n")
-print("Generating error analysis (only input/parameters) dataset")
-print("\n")
-thermal_error_analysis_set = generate_ann_input_set(samples=error_analysis_samples)
-thermal_error_numpy = np.zeros(thermal_error_analysis_set.shape[0])
-
-for i in range(thermal_error_analysis_set.shape[0]):
-    print(f"Error analysis {i+1} of {thermal_error_analysis_set.shape[0]}")
-    print(f"Parameter: : {thermal_error_analysis_set[i,:]}")
-    thermal_error_numpy[i] = error_analysis(thermal_reduced_problem, thermal_problem_parametric,
-                                    thermal_error_analysis_set[i, :], thermal_model,
-                                    len(thermal_reduced_problem._basis_functions),
-                                    online_nn)
-    print(f"Error: {thermal_error_numpy[i]}")
-
-# ### Thermal ANN ends
-'''
 mechanical_problem_parametric = \
     MechanicalProblemOnDeformedDomain(mesh, cell_tags, facet_tags,
                                       thermal_problem_parametric)
-
-# solution_mu = mechanical_problem_parametric.solve(mu_ref)
-# print(f"Solution norm at mu:{mu_ref}: {mechanical_problem_parametric.inner_product_action(solution_mu)(solution_mu)}")
 
 solution_mu = mechanical_problem_parametric.solve(mu)
 print(f"Solution norm at mu:{mu}: {mechanical_problem_parametric.inner_product_action(solution_mu)(solution_mu)}")
@@ -562,7 +329,16 @@ with MeshDeformationWrapperClass(mesh, facet_tags, mu_ref, mu):
 
 # ### Mechanical POD starts ###
 
-# NOTE using same generate_training_set as Thermal problem
+def generate_training_set(samples=pod_samples):
+    training_set_0 = np.linspace(0.55, 0.75, samples[0])
+    training_set_1 = np.linspace(0.35, 0.55, samples[1])
+    training_set_2 = np.linspace(0.8, 1.2, samples[2])
+    training_set_3 = np.linspace(0.4, 0.6, samples[3])
+    training_set = np.array(list(itertools.product(training_set_0,
+                                                   training_set_1,
+                                                   training_set_2,
+                                                   training_set_3)))
+    return training_set
 
 mechanical_training_set = rbnicsx.io.on_rank_zero(mesh.comm, generate_training_set)
 
@@ -626,6 +402,35 @@ plt.savefig("mechanical_eigenvalues.png")
 print(f"Eigenvalues (Mechanical): {mechanical_positive_eigenvalues}")
 
 # ### Mechanical POD Ends ###
+
+def generate_ann_input_set(samples=ann_samples):
+    """Generate an equispaced training set using numpy."""
+    training_set_0 = np.linspace(0.55, 0.75, samples[0])
+    training_set_1 = np.linspace(0.35, 0.55, samples[1])
+    training_set_2 = np.linspace(0.8, 1.2, samples[2])
+    training_set_3 = np.linspace(0.4, 0.6, samples[3])
+    training_set = np.array(list(itertools.product(training_set_0,
+                                                   training_set_1,
+                                                   training_set_2,
+                                                   training_set_3)))
+    return training_set
+
+
+def generate_ann_output_set(problem, reduced_problem,
+                            input_set, mode=None):
+    output_set = np.zeros([input_set.shape[0],
+                           len(reduced_problem._basis_functions)])
+    for i in range(input_set.shape[0]):
+        if mode is None:
+            print(f"Parameter number {i+1} of {input_set.shape[0]}")
+            print(f"Parameter: {input_set[i,:]}")
+        else:
+            print(f"{mode} parameter number {i+1} of {input_set.shape[0]}")
+            print(f"Parameter: {input_set[i,:]}")
+        output_set[i, :] = \
+            reduced_problem.project_snapshot(problem.solve(input_set[i, :]),
+                                             len(reduced_problem._basis_functions)).array.astype("f")
+    return output_set
 
 # Training dataset
 mechanical_ann_input_set = generate_ann_input_set(samples=ann_samples)
@@ -729,63 +534,7 @@ for i in range(mechanical_error_analysis_set.shape[0]):
 
 # ### Online phase ###
 online_mu = np.array([0.45, 0.56, 0.9, 0.7])
-'''
-thermal_fem_solution = thermal_problem_parametric.solve(online_mu)
-thermal_rb_solution = \
-    thermal_reduced_problem.reconstruct_solution(
-        online_nn(thermal_reduced_problem, thermal_problem_parametric,
-                  online_mu, thermal_model,
-                  len(thermal_reduced_problem._basis_functions)))
 
-thermal_fem_solution_plot = dolfinx.fem.Function(VT_plot)
-thermal_fem_solution_plot.interpolate(thermal_fem_solution)
-
-thermal_rb_solution_plot = dolfinx.fem.Function(VT_plot)
-thermal_rb_solution_plot.interpolate(thermal_rb_solution)
-
-thermal_fem_online_file \
-    = "dlrbnicsx_solution_thermomechanical/thermal_fem_online_mu_computed.xdmf"
-with MeshDeformationWrapperClass(mesh, facet_tags, mu_ref,
-                           online_mu):
-    with dolfinx.io.XDMFFile(mesh.comm, thermal_fem_online_file,
-                             "w") as thermal_solution_file:
-        thermal_solution_file.write_mesh(mesh)
-        thermal_solution_file.write_function(thermal_fem_solution_plot)
-
-thermal_rb_online_file \
-    = "dlrbnicsx_solution_thermomechanical/thermal_rb_online_mu_computed.xdmf"
-with MeshDeformationWrapperClass(mesh, facet_tags, mu_ref,
-                           online_mu):
-    with dolfinx.io.XDMFFile(mesh.comm, thermal_rb_online_file,
-                             "w") as thermal_solution_file:
-        # NOTE scatter_forward not considered for online solution
-        thermal_solution_file.write_mesh(mesh)
-        thermal_solution_file.write_function(thermal_rb_solution_plot)
-
-thermal_error_function = dolfinx.fem.Function(thermal_problem_parametric._VT)
-thermal_error_function.x.array[:] = \
-    thermal_fem_solution.x.array - thermal_rb_solution.x.array
-
-thermal_error_function_plot = dolfinx.fem.Function(VT_plot)
-thermal_error_function_plot.interpolate(thermal_error_function)
-
-thermal_fem_rb_error_file \
-    = "dlrbnicsx_solution_thermomechanical/thermal_fem_rb_error_computed.xdmf"
-with MeshDeformationWrapperClass(mesh, facet_tags, mu_ref,
-                           online_mu):
-    with dolfinx.io.XDMFFile(mesh.comm, thermal_fem_rb_error_file,
-                             "w") as thermal_solution_file:
-        thermal_solution_file.write_mesh(mesh)
-        thermal_solution_file.write_function(thermal_error_function_plot)
-
-with MeshDeformationWrapperClass(mesh, facet_tags, mu_ref,
-                           online_mu):
-    print(thermal_reduced_problem.norm_error(thermal_fem_solution, thermal_rb_solution))
-    print(thermal_reduced_problem.compute_norm(thermal_error_function))
-
-print(thermal_reduced_problem.norm_error(thermal_fem_solution, thermal_rb_solution))
-print(thermal_reduced_problem.compute_norm(thermal_error_function))
-'''
 mechanical_fem_solution = mechanical_problem_parametric.solve(online_mu)
 mechanical_rb_solution = \
     mechanical_reduced_problem.reconstruct_solution(
@@ -838,5 +587,4 @@ with MeshDeformationWrapperClass(mesh, facet_tags, mu_ref,
     print(mechanical_reduced_problem.norm_error(mechanical_fem_solution, mechanical_rb_solution))
     print(mechanical_reduced_problem.compute_norm(mechanical_error_function))
 
-# print(f"Training time (Thermal): {thermal_elapsed_time}")
 print(f"Training time (Mechanical): {mechanical_elapsed_time}")
