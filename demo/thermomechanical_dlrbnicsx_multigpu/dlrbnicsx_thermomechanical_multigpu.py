4dcfbcf31e2d2f9aac17225b284fa869a3eb3abf
niravshah241 <niravshahcolab@gmail.com>
2023-12-21 14:10:08 +0000

    thermomech gpu in work

--- demo/thermomechanical_dlrbnicsx_multigpu/dlrbnicsx_thermomechanical_multigpu.py
+++ demo/thermomechanical_dlrbnicsx_multigpu/dlrbnicsx_thermomechanical_multigpu.py
@@ -588,7 +588,7 @@ if mechanical_gpu_group0_comm != MPI.COMM_NULL:
     mechanical_checkpoint_path = "mechanical_checkpoint"
     mechanical_checkpoint_epoch = 10
 
-    mechanical_learning_rate = 1e-4
+    mechanical_learning_rate = 1e-6
     mechanical_optimiser = get_optimiser(mechanical_model, "Adam", mechanical_learning_rate)
     mechanical_loss_fn = get_loss_func("MSE", reduction="sum")
 
