6565ec26d437e227a3dfec66f69a41fd42df9f54
niravshah241 <niravshahcolab@gmail.com>
2024-11-01 19:24:53 +0000

    [skip ci] Saddle point distributed in work

--- demo/saddle_point_dlrbnicsx_distributed/dlrbnicsx_saddle_point_distributed.py
+++ demo/saddle_point_dlrbnicsx_distributed/dlrbnicsx_saddle_point_distributed.py
@@ -1432,7 +1432,6 @@ for j in range(len(fem_comm_list)):
                                    index=0
                                 )
                 print(f"Error analysis (sigma) {i+1} of {error_analysis_set.shape[0]}, Model {array_num}, Error: {error_array_list_sigma[array_num][i]}")
-exit()
 
 if world_comm.size == 8:
     cpu_group0_procs_u = world_comm.group.Incl([0, 1])
@@ -1797,6 +1796,7 @@ for j in range(len(fem_comm_list)):
                                    index=1
                                 )
                 print(f"Error analysis (U) {i+1} of {error_analysis_set.shape[0]}, Model {array_num}, Error: {error_array_list_u[array_num][i]}")
+exit()
 
 if fem_comm_list[0] != MPI.COMM_NULL:
     # Online phase at parameter online_mu
