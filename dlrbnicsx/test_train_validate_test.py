7265ef2089248783967255d6dd7ed1ce5c4a5e2a
niravshah241 <nvs31@UMD-ADMIN270256.localdomain>
2025-10-10 14:50:45 +0100

    Training Validation serial part in testing

--- dlrbnicsx/test_train_validate_test.py
+++ dlrbnicsx/test_train_validate_test.py
@@ -167,7 +167,8 @@ class TestTrainValidateTest(unittest.TestCase):
         reduced_problem.output_range[0] = np.min(output_training_data)
         reduced_problem.output_range[1] = np.max(output_training_data)
 
-        indices = np.arange(comm.rank, num_para, comm.size)
+        indices = np.arange(comm.rank, input_training_data.shape[0],
+                            comm.size)
 
         custom_partitioned_dataset = \
             CustomPartitionedDataset(reduced_problem,
